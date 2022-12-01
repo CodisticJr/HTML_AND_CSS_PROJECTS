ec1dc7b2114f39bfbde9a78b6a70c04876c5cf1b
CodisticJr <115127368+CodisticJr@users.noreply.github.com>
2022-12-01 10:14:04 -0800

    Photo Displayed on page

---
+++ Basic_HTML_AND_JavaScript/Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+alert("Warning: ");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+
+function Sun_Peaks() {
+	var Image = "C:\Users\Tim Shaw\myProjects\Projects-of-HTML-and-CSS\HTML documents\SunPeaksSunSet.jpg";
+	document.getElementById("Photo1").innerHTML = 
+	Image;
+}
