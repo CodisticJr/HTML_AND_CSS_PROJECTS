8d5f569b8c91932e57bbcf94ce0abf3763671566
CodisticJr <115127368+CodisticJr@users.noreply.github.com>
2022-12-14 20:23:04 -0800

    Linking HTML and JavaScript
    Using separate files to house HTML docs and JavaScript docs for storage

--- Basic_HTML_AND_JavaScript/Basic_JavaScript_1.js
+++ Basic_HTML_AND_JavaScript/Basic_JavaScript_1.js
@@ -1,4 +1,4 @@
-alert("Warning: ");
+alert("Warning: This is How to Show an Alert box complete with 'OK' button ");
 function My_First_Function() {
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
