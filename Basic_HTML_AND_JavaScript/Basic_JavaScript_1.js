29861c06686e38cd16cd281f84ea93e4c352656a
CodisticJr <115127368+CodisticJr@users.noreply.github.com>
2022-12-14 22:01:37 -0800

    Basic_HTML_1.html Check back
    displaying image with a function, well trying to, used html comments to store data with project.

--- Basic_HTML_AND_JavaScript/Basic_JavaScript_1.js
+++ Basic_HTML_AND_JavaScript/Basic_JavaScript_1.js
@@ -1,13 +1,7 @@
 alert("Warning: This is How to Show an Alert box complete with 'OK' button ");
-function My_First_Function() {
+function My_First_Function() { 
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
 		document.getElementById("Irish").innerHTML =
 		result;
 	}
-
-function Sun_Peaks() {
-	var Image = "C:\Users\Tim Shaw\myProjects\Projects-of-HTML-and-CSS\HTML documents\SunPeaksSunSet.jpg";
-	document.getElementById("Photo1").innerHTML = 
-	Image;
-}
